b3de824dd545306e20a1734519d2bb343536c1a4
Mari <vergunmari@gmail.com>
2020-03-16 22:05:41 +0300

    feat: add toggle functions

--- index.js
+++ index.js
@@ -9,3 +9,26 @@ function scrollElement(scrollTo) {
     scrollTo.scrollIntoView({block: "start", behavior: "smooth"});
 }
 /********SCROLL ELEMENTS: END********/
+
+
+
+/********TOGGLE: START********/
+
+function toggleClasses(event) {
+    event.stopPropagation();
+    const {currentTarget: container, target: activeEl} = event;
+    if (container.querySelector('.active')) {
+        container.querySelector('.active').classList.remove('active');
+    }
+    activeEl.classList.toggle('active');
+}
+
+function toggleButtons(event) {
+    event.stopPropagation();
+    const {currentTarget: container, target: activeEl} = event;
+    toggleClasses(event);
+    console.log(activeEl.dataset.filter);
+    container.closest('.content-section').querySelector('.portfolio-grid').dataset.filter = activeEl.dataset.filter;
+}
+
+/********TOGGLE: START********/
