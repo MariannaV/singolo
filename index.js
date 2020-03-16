872784b034a1221b28cba2879b37ca41a97802a9
Mari <vergunmari@gmail.com>
2020-03-16 22:06:33 +0300

    feat: add slider

--- index.js
+++ index.js
@@ -32,3 +32,63 @@ function toggleButtons(event) {
 }
 
 /********TOGGLE: START********/
+
+
+/********SLIDER: START********/
+
+const sliders = {};
+
+function handleNavigationButtons(button, {isNextButton, isPrevButton} = {}) {
+    const slider = button.closest('.slider'),
+        slidesContainer = slider.querySelector('.slides-container'),
+        slidesWidth = slidesContainer.children[1].offsetWidth,
+        slidesGap = +getComputedStyle(slidesContainer).getPropertyValue('--horizontal-gap');
+
+    if (!(slider.id in sliders))
+        sliders[slider.id] = {
+            currentSlideIndex: 1,
+            buttonPrev: slider.querySelector('.slider-nav-prev'),
+            buttonNext: slider.querySelector('.slider-nav-next')
+        };
+
+    let {currentSlideIndex} = sliders[slider.id];
+
+    const isNeedWillMove = () => {
+        switch (true) {
+            case isNextButton:
+                return currentSlideIndex < slidesContainer.children.length - 2;
+            case isPrevButton:
+                return currentSlideIndex > 1;
+            default:
+                throw new Error('Некорректный тип кнопки');
+        }
+    };
+
+    if (isNeedWillMove()) {
+        if (isNextButton)
+            currentSlideIndex++;
+        else if (isPrevButton)
+            currentSlideIndex--;
+
+        slidesContainer.scrollLeft = (slidesWidth + slidesGap) * currentSlideIndex;
+    } else {
+        slidesContainer.classList.add('showFake');
+        slidesContainer.classList.add('disableSmooth');
+
+        if (isNextButton) {
+            slidesContainer.scrollLeft = 0;
+            slidesContainer.classList.remove('disableSmooth');
+            currentSlideIndex = 1;
+        } else if (isPrevButton) {
+            slidesContainer.scrollLeft = (slidesWidth + slidesGap) * (slidesContainer.children.length - 1);
+            slidesContainer.classList.remove('disableSmooth');
+            currentSlideIndex = slidesContainer.children.length - 2;
+        }
+
+        slidesContainer.scrollLeft = (slidesWidth + slidesGap) * currentSlideIndex;
+    }
+    
+    sliders[slider.id].currentSlideIndex = currentSlideIndex;
+}
+
+/********SLIDER: END********/
